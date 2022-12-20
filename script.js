ff837096bcf1d1efd571863f80fdf94fc3b66e9a
Demitra <109766095+demitraps@users.noreply.github.com>
2022-12-20 14:45:08 +0200

    Create Tarot Card array

--- script.js
+++ script.js
@@ -1,1 +1,142 @@
+/* Source of Tarot card list: https://www.twowander.com/blog/list-of-tarot-cards 
+and https://www.biddytarot.com/tarot-card-meanings/major-arcana/wheel-of-fortune/ */
 
+// 22 cards with upright and reversed keywords
+
+const majorArcanaTarotCards = [
+  {
+    name: "The Fool",
+    meaning: "new beginnings",
+    upright:
+      "youthful energy, fresh starts, new beginnings, naïveté, start of a cycle",
+    reversed: "foolish, jumping in before looking, getting ahead of yourself",
+  },
+  {
+    name: "The Magician",
+    meaning: "manifestation",
+    upright: "creation, manifestation, capability",
+    reversed: "misalignment, confusion, destruction",
+  },
+  {
+    name: "The High Priestess",
+    meaning: "intuition",
+    upright: "intuition",
+    reversed: "blocked intuition",
+  },
+  {
+    name: "The Empress",
+    meaning: "nurturing",
+    upright: "mothering, nurturance, nourishment, luxury, indulgence",
+    reversed: "lack of self-care, need to slow down and be present",
+  },
+  {
+    name: "The Empreror",
+    meaning: "leadership",
+    upright: "authority, power, leadership, confidence, determination",
+    reversed: "overbearing, overpowering, headstrong, dictatorial",
+  },
+  {
+    name: "The Hierophant",
+    meaning: "teachings",
+    upright: "religion, spirituality, doctrine, teachings",
+    reversed: "dogma",
+  },
+  {
+    name: "The Lovers",
+    meaning: "duality",
+    upright: "relationships, love, romance, partnerships, duality",
+    reversed: "blind love, giving away of power, loss of self",
+  },
+  {
+    name: "The Chariot",
+    meaning: "endurance",
+    upright: "strength, perseverance, endurance, emotional stability",
+    reversed: "opposition, lack of direction",
+  },
+  {
+    name: "Strength",
+    meaning: "courage",
+    upright: "strength, courage, bravery, vulnerability",
+    reversed: "aggression, forcing, hotheaded",
+  },
+  {
+    name: "The Hermit",
+    meaning: "introspection",
+    upright: "introspection, reflection, solace, insight, wisdom, retreat",
+    reversed: "loneliness",
+  },
+  {
+    name: "Wheel of Fortune",
+    meaning: "luck",
+    upright:
+      "luck, fortune, changing cycles, wheel of life, cosmic dance, Divine timing, fate, destiny",
+    reversed: "bad luck, resistance to change, breaking cycles",
+  },
+  {
+    name: "Justice",
+    meaning: "equity",
+    upright:
+      "equality, equity, fairness, adroitness, righteousness, morals, ethics",
+    reversed: "unfairness, lack of accountability, dishonesty",
+  },
+  {
+    name: "The Hanged Man",
+    meaning: "pause",
+    upright: "wait, pause, surrender, inaction",
+    reversed: "delays, resistance, stalling, indecision",
+  },
+  {
+    name: "Death",
+    meaning: "transformation",
+    upright: "change, transformation, transmutation, end of a cycle, rebirth",
+    reversed: "resistance to change, personal transformation, inner purging",
+  },
+  {
+    name: "Temperance",
+    meaning: "patience",
+    upright: "balance, moderation, patience, purpose",
+    reversed: "imbalance, excess, self-healing, re-alignment",
+  },
+  {
+    name: "The Devil",
+    meaning: "obsessions",
+    upright: "shadow self, attachment, addiction, restriction, sexuality",
+    reversed: "releasing limiting beliefs, exploring dark thoughts, detachment",
+  },
+  {
+    name: "The Tower",
+    meaning: "upheaval",
+    upright: "sudden change, upheaval, chaos, revelation, awakening",
+    reversed: "personal transformation, fear of change, averting disaster",
+  },
+  {
+    name: "The Star",
+    meaning: "hope",
+    upright: "hope, reset, refresh, regeneration, faith, trust",
+    reversed: "loss of hope, loss of faith",
+  },
+  {
+    name: "The Moon",
+    meaning: "illusion",
+    upright: "intuition, illusions, confusion, secrets, mysteries, unknown",
+    reversed: "lies, deceit",
+  },
+  {
+    name: "The Sun",
+    meaning: "abundance",
+    upright: "positivity, fun, warmth, success, vitality",
+    reversed: "inner child, feeling down, overly optimistic",
+  },
+  {
+    name: "Judgement",
+    meaning: "abundance",
+    upright: "judgement, rebirth, inner calling, absolution",
+    reversed: "self-doubt, inner critic, ignoring the call",
+  },
+  {
+    name: "The World",
+    meaning: "completion",
+    upright: "completion, integration, accomplishment, travel",
+    reversed: "seeking personal closure, short-cuts, delays",
+  },
+];
